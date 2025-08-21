c2839ebfae7ca305f97bb24736640d15bd1a978b
Mujib <mujibidrees@gmail.com>
2025-08-21 16:02:15 +0100

    Updating user responsiveness

--- portfolio.js
+++ portfolio.js
@@ -4,13 +4,7 @@ for (let i = 0; i < selection.length; i++) {
   selection[i].addEventListener("click", activeClass)  
 }
 function activeClass(key){
-    if (key === "active") {
-          selection.classList.add("active");
-          console.log(selection);
-        } else {
-          selection.classList.remove("active");
-        }
-
+selection.classList.add("active")
 }
 
 
